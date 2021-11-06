92ffb6eefb8fedb8065af889d03f05af7fc89272
柏瑜 曾 <zxc1112508@gmail.com>
2021-11-06 21:08:50 +0800

    完成上課內容

--- WpfApp2/MainWindow.xaml.cs
+++ WpfApp2/MainWindow.xaml.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.IO;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Forms;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using RadioButton = System.Windows.Controls.RadioButton;
 
@@ -35,7 +37,7 @@ namespace WpfApp2
                     //畫圖模式,滑鼠左鍵被按住時,持續取得滑鼠座標
                     if (e.LeftButton == MouseButtonState.Pressed)
                     {
-                        MyCanvas.Cursor = System.Windows.Input.Cursors.Pen;
+                        MyCanvas.Cursor = System.Windows.Input.Cursors.Pen;//更改游標為畫筆
                         dest = e.GetPosition(MyCanvas);//終點座標
                         MyLabel.Content = $"座標點:({start})-({dest})";
                     }
@@ -107,6 +109,7 @@ namespace WpfApp2
             MyCanvas.Children.Add(newRectangle);//加入為MyCanvas子項目
         }
 
+        /*重設XY 以不受限特定繪圖方向*/
         private void AdjustPoint()
         {
             double X_min, Y_min, X_max, Y_max;
@@ -161,6 +164,7 @@ namespace WpfApp2
             return Color.FromArgb(dlgColor.A, dlgColor.R, dlgColor.G, dlgColor.B);
         }
 
+        /*畫圖模式共用*/
         private void ShapeButton_Click(object sender, RoutedEventArgs e)
         {
             RadioButton btn = sender as RadioButton;//轉型成button
@@ -168,6 +172,7 @@ namespace WpfApp2
             currentAction = "Draw";//畫圖模式
         }
 
+        /*填滿色彩*/
         private void FillButton_Click(object sender, RoutedEventArgs e)
         {
             currentFillColor = GetDialogColor();
@@ -175,9 +180,63 @@ namespace WpfApp2
             FillButton.Background = currentFillBrush;
         }
 
+        /*橡皮擦功能*/
         private void EraseButton_Click(object sender, RoutedEventArgs e)
         {
             currentAction = "Erase";//清除模式
+        }
+
+        /*隱藏顯示工具列*/
+        private void menuCheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            if (menuCheckBox.IsChecked == true)
+            {
+                MyToolBarTray.Visibility = Visibility.Visible;//顯示工具列
+                MyCanvas.Height -= MyToolBarTray.Height;
+            }
+            else
+            {
+                MyToolBarTray.Visibility = Visibility.Collapsed;//隱藏工具列,並且不保留空位
+                MyCanvas.Height += MyToolBarTray.Height;
+            }
+        }
+
+        /*清空畫布*/
+        private void ClearCanvasButton_Click(object sender, RoutedEventArgs e)
+        {
+            MyCanvas.Children.Clear();//清除所有子項目
+            MyCanvas.Cursor = System.Windows.Input.Cursors.Arrow;//游標改回箭頭
+            MyLabel.Content = "Ready";
+
+        }
+
+        /*儲存畫布*/
+        private void SaveCanvasMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            int w = Convert.ToInt32(MyCanvas.RenderSize.Width);//取得畫布寬度
+            int h = Convert.ToInt32(MyCanvas.RenderSize.Height);//取得畫布高度
+
+            //將MyCanvas轉存成bitmp
+            RenderTargetBitmap rtb = new RenderTargetBitmap(w, h, 64d, 64d, PixelFormats.Default);
+            rtb.Render(MyCanvas);
+
+            //將bitmap編碼成png格式
+            PngBitmapEncoder png = new PngBitmapEncoder();
+            png.Frames.Add(BitmapFrame.Create(rtb));
+
+            //開啟存檔對話方塊
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Title = "儲存小畫家檔案";
+            saveFileDialog.DefaultExt = "*.png";
+            saveFileDialog.Filter = "PNG檔案(*.png)|*.png|全部檔案|*.*";
+            saveFileDialog.ShowDialog();
+            string path = saveFileDialog.FileName;
+
+            //儲存png檔案
+            using (var fs = File.Create(path))
+            {
+                png.Save(fs);
+            }
         }
 
         /*取得起點*/
